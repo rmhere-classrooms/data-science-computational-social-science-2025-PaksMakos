90c3be93c199747d8caf4b846cd3819df6a69aab
Pakos <pakostasip@gmail.com>
2025-11-07 00:13:35 +0100

    Zadanie 1

--- mysolution/mysolution1.R
+++ mysolution/mysolution1.R
@@ -5,5 +5,25 @@ p <- 0.05
 
 g <- sample_gnp(n = n, p = p)
 
-plot(g)
 summary(g)
+# graf nie jest ważony
+
+E(g)
+V(g)
+
+E(g)$weight <- runif(length(E(g)), min = 0.01, max = 1)
+
+summary(g)
+# po dodaniu wag do krawędzi graf jest ważony
+
+degree(g)
+
+hist(degree(g))
+
+# connected components (klastry)
+comp <- components(g)
+cat("Number of connected components:", comp$no, "\n")
+
+pr <- page.rank(g)$vector
+
+plot(g, vertex.size=pr*250, vertex.label=NA, edge.arrow.size=.2)
